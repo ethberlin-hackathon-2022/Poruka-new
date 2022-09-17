748a799f21624f3fac679b93accf65e05bac6279
Yanis Meziane <yanismeziane@Yaniss-MacBook-Pro.local>
2022-09-18 00:56:25 +0200

    Twitter list

--- frontend/src/helpers/fetchFollowers.js
+++ frontend/src/helpers/fetchFollowers.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export default async function fetchFollowers(id) {
-  axios
+  return axios
     .get("https://givewithporuka.pythonanywhere.com/getFollowing?id=" + id)
     .then((res) => {
+      console.log("result within the function", res);
       return res.data.result;
     });
 }
