69692d09114d2db63c098fe21240aa101661526b
Yanis Meziane <yanismeziane@Yaniss-MacBook-Pro.local>
2022-09-17 22:18:33 +0200

    Twitter connect not hardcoded

--- frontend/src/helpers/isTwitterResolved.js
+++ frontend/src/helpers/isTwitterResolved.js
@@ -2,8 +2,6 @@ import { IdrissCrypto } from "idriss-crypto/lib/browser";
 
 export default async function isTwitterResolved(address) {
   const obj = new IdrissCrypto();
-  const reverse = await obj.reverseResolve(
-    "0x4a3755eb99ae8b22aafb8f16f0c51cf68eb60b85"
-  );
+  const reverse = await obj.reverseResolve(address);
   return reverse;
 }
