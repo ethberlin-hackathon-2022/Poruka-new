8d4a9fbc1f6fc6fec8fc20ee457b327482c98bc0
2xic <5640782+2xic@users.noreply.github.com>
2022-09-18 07:54:51 +0200

    Debug message

--- contracts/deploy_mockdai.sh
+++ contracts/deploy_mockdai.sh
@@ -6,4 +6,5 @@ else
     exit 0
 fi
 
-forge create --rpc-url $FOUNDRY_ETH_URL --private-key $FOUNDRY_ETH_PRIVATE_KEY src/DaiMock.sol:DaiMock
+forge create --rpc-url $FOUNDRY_ETH_URL --private-key $FOUNDRY_ETH_PRIVATE_KEY src/DaiMock.sol:MockDai
+forge inspect ./src/DaiMock.sol:MockDai abi > MockDai_abi.json
